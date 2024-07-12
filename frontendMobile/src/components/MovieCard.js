ee4abfbde658aa70303111d8c0a337c66c092012
rihem1999 <rihem.farjallah1999@gmail.com>
2024-07-12 21:07:17 +0100

    design updated

--- frontendMobile/src/components/MovieCard.js
+++ frontendMobile/src/components/MovieCard.js
@@ -1,16 +1,56 @@
-// src/components/MovieCard.js
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 const MovieCard = ({ movie, onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View>
-        <Text>{movie.Title}</Text>
-        <Text>{movie.Year}</Text>
+    <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
+      <Image
+        source={{ uri: movie.Poster }}
+        style={styles.posterImage}
+        resizeMode="cover"
+      />
+      <View style={styles.cardDetails}>
+        <Text style={styles.title}>{movie.Title}</Text>
+        <Text style={styles.year}>{movie.Year}</Text>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  posterImage: {
+    width: 80,
+    height: 120,
+    borderRadius: 8,
+  },
+  cardDetails: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  year: {
+    fontSize: 16,
+    color: '#888',
+  },
+});
 
 export default MovieCard;
